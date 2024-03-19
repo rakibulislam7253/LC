e98112caafa683105fd5c940dec41559fcfcd6ce
atikur.rahaman <atikur.rahaman@citizensbankbd.com>
2024-03-19 13:38:14 +0600

    Project upload

---
+++ src/service/user.service.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const BASE_URL = import.meta.env.VITE_APP_CONTROL_CENTER_API;
+const API_URL = BASE_URL + 'api';
+
+class UserService {
+    getCurrentUserData() {
+        return axios.get(API_URL + '/Login/refresh-user-token', {
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem('accessToken')
+            }
+        });
+    }
+
+    getUserById(userId) {
+        return axios.get(API_URL + '/' + userId);
+    }
+
+    updateUserActiveStatus(userId, publishStatus) {
+        return axios.post(API_URL + '/' + userId + '/change-status/' + publishStatus, null);
+    }
+
+    updateCurrentUser(userData) {
+        return axios.post(API_URL + '/update/current-user', userData);
+    }
+
+    updateUser(user) {
+        return axios.post(API_URL + '/update', user);
+    }
+
+    refreshAdminUserAccessToken() {
+        return axios.get(API_URL + '/Login/refresh-user-token', {
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem('accessToken')
+            }
+        });
+    }
+}
+
+export default new UserService();
